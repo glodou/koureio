a27881fe568bbcfbeb93b6a0501b9c225acc52e3
Gloria Doudou <gloria.dou@hotmail.com>
2026-03-26 23:15:56 +0200

    added the about on index

--- index.php
+++ index.php
@@ -32,4 +32,22 @@ echo "</tr>";
 ?> 
 </table>
 
+<h2> Σχετικά με μας</h2>
+
+
+<div class="about-container">
+    <?php 
+$ab="SELECT* FROM about ORDER BY about_id DESC LIMIT 1";
+$res=mysqli_query($conn,$ab);
+$row=mysqli_fetch_assoc($res);
+    ?>
+
+    <div class="col">
+    <h2><?php echo $row['title']; ?></h2>
+    <p><?php echo $row['content']; ?></p>
+
+    </div>
+
+</div>    
+
 <?php include_once 'includes/footer.php'; ?>
